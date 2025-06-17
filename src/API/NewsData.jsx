e79ae2a9392e1ef3b58a08dd22a73ecca19fd300
Krutarth-Raval <ravalkrutarth95@gmail.com>
2025-06-17 12:45:58 +0530

    update NewsData.jsx file

--- src/API/NewsData.jsx
+++ src/API/NewsData.jsx
@@ -1,13 +1,14 @@
 export const NewsData = async () => {
   try {
-    const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?category=entertainment&language=en&apiKey=${
-        import.meta.env.VITE_NEWS_API_KEY
-      }`
-    );
+    const proxy = "https://api.allorigins.win/raw?url=";
+    const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+    const url = `https://newsapi.org/v2/top-headlines?category=entertainment&language=en&apiKey=${apiKey}`;
+    
+    const response = await fetch(proxy + encodeURIComponent(url));
     const data = await response.json();
-    return data.articles; // Returns an array of news articles
+    return data.articles;
   } catch (error) {
     console.error("Error fetching movie news:", error);
+    return [];
   }
 };
